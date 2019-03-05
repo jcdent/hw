908fb193adb30bab57c059e277901edeaf94e277
JC Dent 2 <some.email@gmail.com>
2019-03-05 22:05:06 +0100

    Use glide and some libs

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"github.com/olekukonko/tablewriter"
+	"os"
+)
 
 func main() {
-	fmt.Println("λ Hello wörld λ 2.0")
+	data := [][]string{
+		[]string{"A", "The Good", "500"},
+		[]string{"B", "The Very very Bad Man", "288"},
+		[]string{"C", "The Ugly", "120"},
+		[]string{"D", "The Gopher", "800"},
+	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Name", "Sign", "Rating"})
+
+	for _, v := range data {
+		table.Append(v)
+	}
+	table.Render() // Send output
 }
